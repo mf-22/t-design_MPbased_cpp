67d2c7e5ff77f92e0b72a46bf2e26db7dfa1b1b8
mf-22 <masahirofujii000@gmail.com>
2021-08-11 13:43:16 +0900

    modify script

--- setup_win.bat
+++ setup_win.bat
@@ -1,7 +1,7 @@
-mkdir data_gen/result
-mkdir fp/result
-mkdir ml/datasets
+mkdir data_gen\result
+mkdir fp\result
+mkdir ml\datasets
 
 git clone https://github.com/qulacs/qulacs.git
 cd qulacs
-.\script\build_msvc_2017.bat
+rem .\script\build_msvc_2017.bat
